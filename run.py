e708088d1766d708db34e89a75bca39fa15c2e03
Axeh99 <jtorreo@hotmail.com>
2020-08-05 13:44:01 +0200

    Add db.create_all()

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from app.localhost import create_app
+from app.localhost import create_app, db
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+db.create_all(app=create_app())
