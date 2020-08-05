0089f830a9e8ab17ce8168f27f7366f476fb08b6
sralloza <sralloza@gmail.com>
2020-08-05 21:33:58 +0200

    Supress FSADeprecationWarning of sqlalchemy

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
     MAIL_PORT = 587
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USER')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
