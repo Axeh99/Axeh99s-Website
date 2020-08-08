83ac792dbfd25ba435b1c45b7b1a013ce6a86c48
sralloza <sralloza@gmail.com>
2020-08-09 00:41:59 +0200

    refactor: rewrite utils.save_picture

--- app/users/utils.py
+++ app/users/utils.py
@@ -1,4 +1,6 @@
-import os
+"""Common operations related to users."""
+
+from pathlib import Path
 import secrets
 
 from PIL import Image
@@ -10,18 +12,16 @@ from app import mail
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(
-        current_app.root_path, "static/profile_pics", picture_fn
-    )
+    picture_path = random_hex + Path(form_picture.filename).suffix
+    pictures_folder = Path(current_app.root_path).joinpath("static/profile_pics")
+    picture_path = pictures_folder / picture_path
 
     output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
+    image = Image.open(form_picture)
+    image.thumbnail(output_size)
+    image.save(picture_path)
 
-    return picture_fn
+    return picture_path
 
 
 def send_reset_email(user):
@@ -31,8 +31,12 @@ def send_reset_email(user):
         sender="elesmoquinado@gmail.com",
         recipients=[user.email],
     )
-    msg.body = f"""To reset your password, visit the following link:
-{url_for('users.reset_token', token=token, _external=True)}
-If you did not make this request then simply ignore this email and no changes will be made.
-"""
+    message = (
+        "To reset your password, visit the following link:\n"
+        f"{url_for('users.reset_token', token=token, _external=True)}\n"
+        "If you did not make this request then simply ignore this email "
+        "and no changes will be made."
+    )
+
+    msg.body = message
     mail.send(msg)
