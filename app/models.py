85754620cab72a7d43150fda9665199c33a400d1
sralloza <sralloza@gmail.com>
2020-08-09 13:07:36 +0200

    docs: add docstrings for database models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+"""Database models used in the website."""
+
 from datetime import datetime
 
 from flask import current_app
@@ -8,11 +10,31 @@ from app import db, login_manager
 
 
 @login_manager.user_loader
-def load_user(user_id):
+def load_user(user_id: int) -> "User":
+    """Returns the user given its id.
+
+    Args:
+        user_id (int): user id.
+
+    Returns:
+        User: user selected by user's id.
+    """
+
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
+    """Represents a valid user.
+
+    Args:
+        id (int): id of the user.
+        username (str): username of the user.
+        email (str): email of the user.
+        image_file (str): path of the user's icon.
+        password (str): password of the user.
+        posts (Post): posts written by the user.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -20,7 +42,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
-    def get_reset_token(self, expires_sec=1800):
+    def get_reset_token(self, expires_sec=1800) -> str:
+        """Generates a password reset token for the user.
+
+        Args:
+            expires_sec (int, optional): Number of seconds in which the reset
+                token is valid. Defaults to 1800.
+
+        Returns:
+            str: reset token generated for the user.
+        """
+
         serial = Serializer(current_app.config["SECRET_KEY"], expires_sec)
         return serial.dumps({"user_id": self.id}).decode("utf-8")
 
@@ -39,6 +71,16 @@ class User(db.Model, UserMixin):
 
 
 class Post(db.Model):
+    """Represents a post in the website.
+
+    Args:
+        id (int): id of the post.
+        title (str): title of the post.
+        date_posted (datetime.datetime): timestamp of the post creation.
+        content (str): content of the post.
+        user_id (id): id of the post owner.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
