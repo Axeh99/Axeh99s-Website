ae674c76fc8eca5e407a2835ca3c85155582367a
sralloza <sralloza@gmail.com>
2020-08-24 22:10:01 +0200

    fix: move database to root folder

--- app/config.py
+++ app/config.py
@@ -10,11 +10,14 @@ load_dotenv(dotenv_path)
 
 
 class Config:
+    ROOT_PATH = Path(__file__).parent.parent.absolute()
     MAIL_PASSWORD = os.environ.get("MAIL_PASS")
     MAIL_PORT = 587
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USER")
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + ROOT_PATH.joinpath("data.sqlite").as_posix()
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
